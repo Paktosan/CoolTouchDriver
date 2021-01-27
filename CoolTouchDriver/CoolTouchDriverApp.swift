87fcf0285e8312797e8f42e016254e03b326a3ed
Julian Fölsch <julianf@paktosan.de>
2021-01-27 15:35:19 +0100

    Initial commit

---
+++ CoolTouchDriver/CoolTouchDriverApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolTouchDriverApp.swift
+//  CoolTouchDriver
+//
+//  Created by Julian Fölsch on 27.01.21.
+//
+
+import SwiftUI
+
+@main
+struct CoolTouchDriverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
